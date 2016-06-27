0159a9fd420a46c6055fd0a4ea9412eeabae8185
Ikkyu0 <pbohnert@gmail.com>
2016-06-27 11:55:04 -0700

    add mongoose for mongodb;  also refactor code to implement my wikimedia API loop and return of JSON

--- app/index.js
+++ app/index.js
@@ -5,17 +5,36 @@ import Utility from '../modules/utility';
 const app = new Express();
 const utility = new Utility();
 
+// set up mongoose
+var mongoose = require('mongoose');
+
+//Setting a 30 second DB connection timeout
+var dbOptions = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }, 
+                replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } };
+
+//other dependencies
+var pathTaken = require("../models/pathTaken.js");
+var moment = require("moment");                
+
+//Connect to our database using the DB server URL.
+mongoose.connect('mongodb://127.0.0.1/philo', dbOptions);
+mongoose.set('debug', true); 
+
 /*
-Only entry point. Change path passed to client to change endpoint
+Our one entry point. Change path passed to client to change endpoint
 */
 app.get('/search', function(req, res) {
-	//console.log(req)
-	//utility.client('/posts/1')
-	utility.client(req.query.url)
-		.then(data =>  {
-		return res.send(data);
-	});
+	//utility.client('/posts/1')  // test url
+	return res.send(utility.findPhilo(req.query.url));
 });
 
 app.listen(4000);
 console.log('Server is running at http://localhost:4000');
+
+/* app.get('/search', function(req, res) {
+	//utility.client('/posts/1')  // test url
+	utility.findPhilo(req.query.url)
+		.then(data =>  {
+		return res.send(data);
+	});
+});*/
--- modules/utility.js
+++ modules/utility.js
@@ -3,8 +3,9 @@
 //dependencies
 import Request from 'request'
 import rp from 'request-promise'
-//var pathTaken = require("../models/pathTaken.js");
-var moment = require("moment");
+
+import pathTaken from '../models/pathTaken.js'
+import moment from 'moment'
 
 const testurl = `http://jsonplaceholder.typicode.com`;
 const origurl = `https://en.wikipedia.org/w/api.php?action=query&titles=Theoretical physics&prop=revisions&rvprop=content&format=json`;
@@ -18,16 +19,58 @@ const options = {
 }
 
 export default class Utility { 
-		client(path) {
+	//findPhilo is our exported function to drive the search for Philosophy
+    findPhilo(path) {
+    	var jsonPage;
+    	var hopCount = 0;
+    	var hopLimit = 5;
+    	var completePath = "Path Taken\n\n";
+    	var searchTerm = path;
+    	var philosophyFound = false;
+    	var title = "";
+    	var nextTerm = "";
+    	var i = 1;
+
+    	while (hopLimit > 0) {
+    		hopCount++;
+    		console.log("looping at hopcount: " + hopCount);
+    		jsonPage = client(searchTerm);
+    	    //console.log(jsonPage);
+    	    // parse JSON to get title, next searchTerm
+    	    // temp code
+    	    nextTerm = "Richard term" + i++;
+    	    title = "Richard Page" + i;
+    	    completePath = completePath + "Title: " + title + "\n";
+    	    if (title == "Philosophy") {
+    	    	philosophyFound = true;
+    	    	break;
+    	    } else if (nextTerm = ""){
+    	    	philosophyFound = false;
+    	    	break;
+    	    } else {
+    	    	searchTerm = nextTerm;
+    	    	hopLimit--;
+    	    }
+    	} // end of while loop
+
+    	saveToDB(completePath, philosophyFound);
+    	var ourJSON = {
+                        "philosophyFound" : philosophyFound, 
+                        "completePath" :  completePath,
+                        "hopCount" : hopCount 
+                                  };
+        return(ourJSON);
+    } // end of class member findPhilo
+}
+
+function client(path) {
 	    return new Promise(
 	      function (resolve, reject) {
-		    	//options.uri = url + path;
-		    	console.log("about to construct url");
 		    	options.uri = url + "&titles=" + path + "&" + urlOptions;
 		    	options.method = 'GET';
-		    	console.log("url is " + path + "and full wikipedia URL is " + options.uri);
+		    	//console.log("url is " + path + "and full wikipedia URL is " + options.uri);
 
-					rp(options)
+				rp(options)
 				    .then(function (response) {
 				        resolve(response);
 				    })
@@ -36,30 +79,30 @@ export default class Utility {
 				       reject(err)
 				    });
 	      });
-		}
-		saveToDB(completePath, philoFound) {
+		} // end of client function
+
+function saveToDB(completePath, philoFound) {
 			var todayDate = moment().startOf('second');
-			 var newPath = new pathTaken(
-                                        {  
-                                             'uniqueRequestID': todayDate,
-                                             'entirePath': completePath,
-                                             'philosophyFound': philoFound,
-                                             'creation_date' :  todayDate
-                                         });  
+			console.log("in saveToDB");
+			var newPath = new pathTaken(
+                                {  
+                                    'uniqueRequestID': todayDate,
+                                    'entirePath': completePath,
+                                    'philosophyFound': philoFound,
+                                    'creation_date' :  todayDate
+                                     });  
 
-                                    newPath.save(function (err, result) {
-                                    if (err) {
-                                                log.error(JSON.stringify(err)); 
-                                                return;
-                                            }
-                                        else {
-                                        	console.log("saved path taken");
-                                        	return;
-                                                  }
-                                       }); // end of save new pathTaken
-		} // end of saveToDB
-
-}
+            newPath.save(function (err, result) {
+                           if (err) {
+                                       log.error(JSON.stringify(err)); 
+                                        return;
+                                       }
+                               else {
+                                        console.log("saved path taken");
+                                        return;
+                                        }
+                         }); // end of save new pathTaken
+	} // end of saveToDB function
 
 
 
